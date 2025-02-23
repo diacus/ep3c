f840a64b8a5ca1a5ce188d14d7e759194e9fa52c
Diacus Mágnuz (from magnuz) <diacus.magnuz@gmail.com>
2025-02-23 13:20:01 -0600

    fix flycheck warnings

--- src/init.el
+++ src/init.el
@@ -1,5 +1,9 @@
-(setq settings-file
-      (if (member system-type '(ms-dos windows-nt cygwin))
-	  (file-name-concat (getenv "HOME") "OneDrive - Microsoft" "emacs" "settings.org")
-	(file-name-concat user-emacs-directory  "settings.org")))
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
+;; End:
+(defvar settings-file
+  (if (member system-type '(ms-dos windows-nt cygwin))
+      (file-name-concat (getenv "HOME") "OneDrive - Microsoft" "emacs" "settings.org")
+    (file-name-concat user-emacs-directory  "settings.org"))
+  "Path to the settings file")
 (org-babel-load-file settings-file)
