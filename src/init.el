eac6c8eaf40b577e79e48c4242873d52014b331f
Diacus Magnuz <diacus.magnuz@gmail.com>
2025-09-11 20:09:51 -0600

    rewrite init.el, split settings into modules, clean up old files

--- src/init.el
+++ src/init.el
@@ -1,9 +1,47 @@
-;; Local Variables:
-;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
-;; End:
-(defvar settings-file
-  (if (member system-type '(ms-dos windows-nt cygwin))
-      (file-name-concat (getenv "HOME") "OneDrive - Microsoft" "emacs" "settings.org")
-    (file-name-concat user-emacs-directory  "settings.org"))
-  "Path to the settings file")
-(org-babel-load-file settings-file)
+;;; init --- Init file for Emacs
+;;; Commentary: simple minimal configuration and module loading
+;;; Code:
+(when (display-graphic-p)
+  (progn
+    (set-scroll-bar-mode nil)
+    (tool-bar-mode 0)))
+(setq column-number-mode t)
+(setq ring-bell-function 'ignore)
+(setq warning-minimum-level :error)
+
+(defconst org-settings-directory
+  (file-name-concat user-emacs-directory "org-settings")
+  "Directory containing 'org-mode' settings modules.")
+
+(package-initialize)
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+
+(package-refresh-contents t)
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(use-package quelpa :ensure t)
+(quelpa '(quelpa-use-package
+	  :fetcher git
+	  :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
+
+
+(defun init/load-settings-module (file)
+  "Execute 'org-babel-load-file' FILE only if file is executable."
+  (when (file-executable-p file)
+      (org-babel-load-file file)))
+
+(let* ((modules-wildcard (file-name-concat user-emacs-directory
+					   "modules"
+					   "*.org"))
+       (modules (file-expand-wildcards modules-wildcard)))
+  (mapc 'init/load-settings-module modules))
+
+(provide 'init)
+;;; init.el
